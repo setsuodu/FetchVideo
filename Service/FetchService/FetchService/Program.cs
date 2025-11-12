17d84c92c90fb013cea0a76983499a402145c420
Xue <setsuodu@users.noreply.github.com>
2025-11-12 18:18:39 +0800

    fix .net camelCase to C# PascalCase

--- Service/FetchService/FetchService/Program.cs
+++ Service/FetchService/FetchService/Program.cs
@@ -1,7 +1,12 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // 关键：禁用 camelCase，保持 C# 命名（PascalCase）
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
 builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(); // 添加服务必须在 app build 之前
