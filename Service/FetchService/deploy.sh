f2ec1aeb138c8d9a1a656488ee933b1542c1385e
Xue <setsuodu@users.noreply.github.com>
2025-11-17 13:27:16 +0800

    更新调试参数

--- Service/FetchService/deploy.sh
+++ Service/FetchService/deploy.sh
@@ -1,17 +1,21 @@
 ﻿#!/bin/bash
 # deploy.sh - 一键构建 + 运行
 
-echo "停止旧容器..."
-docker rm -f downloader || true
+cd FetchVideo/Service/FetchService
 
 echo "构建镜像..."
 docker build -t fetch-service .
 
-echo "启动新容器..."
-docker run -d \
-  --name downloader \
-  -p 8080:8080 \
-  -v /download:/app/downloads \
-fetch-service
+echo "打标签..."
+docker tag fetch-service setsuodu/fetch-service:latest
 
-echo "部署完成！访问 http://localhost:8080/swagger"
+echo "推送..."
+docker push setsuodu/fetch-service:latest
+
+
+# 👇以下在docker里执行
+echo "拉取新镜像..."
+docker pull setsuodu/fetch-service:latest
+
+echo "运行..."
+docker run -d --name downloader -p 8080:8080 -v /vol1/1000/download:/app/downloads setsuodu/fetch-service:latest
