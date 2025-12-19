f035fdfad9f2d19661c3f636a9d2dde0e7a8e4b2
Xue <setsuodu@users.noreply.github.com>
2025-12-19 12:24:54 +0800

    update efcore to 9.0.11

--- Service/Service/Context/AppDbContext.cs
+++ Service/Service/Context/AppDbContext.cs
@@ -26,7 +26,8 @@ public class AppDbContext : DbContext
             entity.HasKey(e => e.Id);
             entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
             entity.Property(e => e.Url).IsRequired().HasMaxLength(500);
-            entity.Property(e => e.Active).HasDefaultValue(true);
+            entity.Property(e => e.Duration).HasDefaultValue(2);
+            entity.Property(e => e.IsSubscribed).HasDefaultValue(false);
         });
 
         base.OnModelCreating(modelBuilder);
